72ff3956781e4510883fad9440d360c82a995225
dwicandra15-svg <dwicandra15@guru.smp.belajar.id>
2026-06-19 05:23:41 +0700

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,4 @@
-importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDKWorker.js');
+// OneSignal Service Worker - diarahkan ke sw.js gabungan
+// File ini dipertahankan untuk kompatibilitas browser yang masih
+// punya registrasi SW lama, tapi isinya sama dengan sw.js
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
